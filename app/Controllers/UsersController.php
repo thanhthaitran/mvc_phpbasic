c75a1527215699afabccead2fefb46a54d914869
Thai.Tran <thai.tran@asiantech.vn>
2018-04-24 11:44:13 +0700

    add new users

--- app/Controllers/UsersController.php
+++ app/Controllers/UsersController.php
@@ -14,6 +14,19 @@
     }
     public function add()
     {
+      if(isset($_POST['add'])){
+        $full_name = $_POST['fullname'];
+        $user_name = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $phone = $_POST['phone'];
+        $add = new User();
+        $result = $add->insert($full_name,$user_name,$email,$password,$phone);
+        if($result){
+          header("LOCATION:index?msg=Bạn đã thêm thành công");
+          exit();
+        }
+      }
       view('users.add-user');
     }
     public function edit()
